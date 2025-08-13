55dba51eaf33111aa3ab4cfa5ca5496d7fa5e521
Praveen Patel <praveenpatel5957@gmail.com>
2025-08-13 14:59:59 +0530

    Fix script path and integrate Locomotive Scroll for smooth scrolling effects

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-function toggleMenu(){
+window.addEventListener("load", () => {
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+    tablet: {
+      smooth: true,
+    },
+    smartphone: {
+      smooth: true,
+    },
+  });
+
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy("[data-scroll-container]", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, { duration: 0, disableLerp: true })
+        : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        left: 0,
+        top: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    pinType: document.querySelector("[data-scroll-container]").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  ScrollTrigger.refresh();
+});
+
+function toggleMenu() {
   const navList = document.querySelector(".nav-list");
 }
